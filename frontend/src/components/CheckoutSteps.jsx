2ba02505ad17abda8c0a7b8f6a078c7a13c91697
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-11-20 19:50:11 +0530

    Update CheckoutSteps.jsx

--- frontend/src/components/CheckoutSteps.jsx
+++ frontend/src/components/CheckoutSteps.jsx
@@ -33,10 +33,10 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
 			<Nav.Item>
 				{step3 ? (
 					<LinkContainer to='/payment'>
-						<Nav.Link>Payment</Nav.Link>
+						<Nav.Link>Payment Method</Nav.Link>
 					</LinkContainer>
 				) : (
-					<Nav.Link disabled>Payment</Nav.Link>
+					<Nav.Link disabled>Payment Method</Nav.Link>
 				)}
 			</Nav.Item>
 
